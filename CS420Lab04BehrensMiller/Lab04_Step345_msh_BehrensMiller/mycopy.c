aaaba17ad4ce008cdfad39fb4bb0c769dece8b92
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2025-12-02 03:38:01 -0600

    Create mycopy.c

---
+++ CS420Lab04BehrensMiller/Lab04_Step345_msh_BehrensMiller/mycopy.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//argv[1] should be source file, argv[2] should be target file
+int main(int argc, char *argv[])
+{
+	if(argc !=3)
+	{
+		fprintf(stderr, "Usage: %s <sourcefile> <targetfile>\n", argv[0]);
+		return 1;
+	}
+	size_t bufSize = 4096;
+	char* buffer = malloc(bufSize);
+	int original = open(argv[1], O_RDONLY, 0644);
+	int copy = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC , 0644);
+	
+	unsigned int bytesRead;
+	
+	while(1)
+	{
+		bytesRead = read(original,buffer, bufSize);
+		if(bytesRead == 0)
+			break;
+		write(copy, buffer, bytesRead);
+	}
+	
+	close(copy);
+	close(original);
+	free(buffer);
+	return 0;
+}
